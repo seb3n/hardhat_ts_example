2bb8155b5ff84fd5dd91302c3213d5d0d4ac2166
pink <8978497+seb3n@users.noreply.github.com>
2021-09-16 00:36:35 -0400

    + abi exporter package

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,12 +2,15 @@
 // dotEnvConfig();
 require('dotenv').config();
 
+
 import { HardhatUserConfig } from 'hardhat/types';
 
 // Plugins
 import '@typechain/hardhat';
 import '@nomiclabs/hardhat-ethers';
 import '@nomiclabs/hardhat-waffle';
+import "hardhat-abi-exporter";
+
 
 const INFURA_API_KEY = process.env.INFURA_API_KEY || '';
 const RINKEBY_PRIVATE_KEY =
@@ -15,7 +18,16 @@ const RINKEBY_PRIVATE_KEY =
   '0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3'; // well known private key
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
-const config: HardhatUserConfig = {
+ 
+
+const config: HardhatUserConfig = {abiExporter: {
+  path: 'data/abi',
+  clear: true,
+  flat: true,
+  only: ['Item$'],
+  spacing: 2,
+  pretty: true,
+},
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {},
@@ -38,6 +50,9 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [{ version: '0.8.4', settings: {} }],
   },
+
+  
 };
 
 export default config;
+
